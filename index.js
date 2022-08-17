de1feee58422135701aa78338e6c90dbd6d52cec
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-18 01:13:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(funActivity = 'roller-skate'){
+    return `This Saturday, I want to ${funActivity}!`
+}
+
+const mondayWork = function(mondayActivity = 'go to the office'){
+    return `This Monday, I will ${mondayActivity}.`
+}
+
+function wrapAdjective(style = '*'){
+
+    return function(adjective = "special"){
+      return `You are ${style}${adjective}${style}!`
+    }
+    
+  }
+  wrapAdjective()();
